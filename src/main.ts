4cc2aa5e49cfdda7ae4f7678ef091756a7f82127
Victor Lobato <victorlobato2310@gmail.com>
2023-03-09 17:01:31 -0300

    adicionando cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200'],
